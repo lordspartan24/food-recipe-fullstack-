b3e0dcb63ab2408b2dca5f149e6e1524d07fd2de
theedwin <118519413+lordspartan24@users.noreply.github.com>
2025-03-10 11:47:32 +0530

    Update Login.jsx

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -12,7 +12,7 @@ export const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/login', { email, password });
+      const response = await axios.post('https://recipecorner-4737.onrender.com/login', { email, password });
 
       if (response.data.token) {
         localStorage.setItem('token', response.data.token); // Save token
